58e2756a62c3719218e0397b54ddfdf3fbcbf2db
Rushabh Shah <rushabhshah122000@gmail.com>
2023-08-02 07:27:51 +0530

    settings edited

--- AI_ChatBot_Project/core/core/settings.py
+++ AI_ChatBot_Project/core/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-puhd4j92&82^rf4d!%0=_thddr_n705o7kj9+7gqn@p8^p#r9j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
