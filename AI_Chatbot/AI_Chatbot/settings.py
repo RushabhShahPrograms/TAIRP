fa10cd714080f5caf5952e8bbd72e24afdb92317
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2023-08-10 20:53:45 +0530

    Update settings.py

--- AI_Chatbot/AI_Chatbot/settings.py
+++ AI_Chatbot/AI_Chatbot/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6k)o-v45*f)jmhf_n91cb!kgy%d#6k!ftw%@^a)vr_+&u8^0s@'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
