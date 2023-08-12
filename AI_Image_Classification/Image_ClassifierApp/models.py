70edc1789f904bc6872b81de83aa889c1ba4200f
Rushabh Shah <rushabhshah122000@gmail.com>
2023-08-12 22:05:32 +0530

    classification website deleted

--- AI_Image_Classification/Image_ClassifierApp/models.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> 0fb0c66000e07c1d11087329b62690269a8180e7
